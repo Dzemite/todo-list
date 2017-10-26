41d1de3fc160b02e2462e1b1ec7be0f6da4b4616
Roman_Nikolaev@epam.com <Roman_Nikolaev@epam.com>
2017-10-26 19:34:35 +0300

    Add tests for todo categories component

--- src/test/todo-categories.component.spec.ts
+++ src/test/todo-categories.component.spec.ts
@@ -7,15 +7,24 @@ import { RouterStub } from "../testing-helpers/router-stubs";
 import { CategoriesService } from "../app/todo/todo-services/categories.service";
 import { HttpModule } from "@angular/http";
 import { TodoListService } from "../app/todo/todo-services/todo-list.service";
+import { Observable } from "rxjs";
+import { Category } from "../app/todo/todo-categories/category";
 
 
 describe('CategoriesComponent', () => {
   let fixture: ComponentFixture<CategoriesComponent>,
       component: CategoriesComponent,
       de: DebugElement,
-      el: HTMLElement;
+      el: HTMLElement,
 
+      categoriesService: CategoriesService,
+      spy: jasmine.Spy;
 
+  const testCategories: Category[] = [
+    {_id: '0', name: 'category 1'},
+    {_id: '1', name: 'category 2'},
+    {_id: '2', name: 'category 3'}
+  ];
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -37,11 +46,34 @@ describe('CategoriesComponent', () => {
     component = fixture.componentInstance;
     de = fixture.debugElement.query(By.css('h3'));
     el = de.nativeElement;
+
+    categoriesService = fixture.debugElement.injector.get(CategoriesService);
+    spy = spyOn(categoriesService, 'getCategories')
+      .and.returnValue(Observable.of(testCategories));
+
   });
 
-  it('should render "Categories"', () => {
+  it('Should render "Categories"', () => {
     fixture.detectChanges();
     expect(el.textContent).toContain('Categories');
   });
+
+  it('Should get array of Category', async(() => {
+    fixture.detectChanges();
+
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+
+      expect(spy.calls.any()).toBe(true, 'getCategories called');
+
+      expect(component.categories[0]._id).toBe('0');
+      expect(component.categories[1]._id).toBe('1');
+      expect(component.categories[2]._id).toBe('2');
+
+      expect(component.categories[0].name).toBe('category 1');
+      expect(component.categories[1].name).toBe('category 2');
+      expect(component.categories[2].name).toBe('category 3');
+    });
+  }));
 });
 
